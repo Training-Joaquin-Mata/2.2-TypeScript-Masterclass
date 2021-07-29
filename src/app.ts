97d52eff9301f0dbf004daa789abd15ceebb0415
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-29 14:06:16 -0500

    ReadOnly Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,21 @@
+//<---------------- ReadOnly Mapped Type
+interface Person{
+    name: string;
+    age: number;
+};
 
-//<---------------- “typeof” Type Queries
-// const person={
-//     name: 'Joaquin',
-//     age: 24
-// };
-
-// type Person = typeof person; 
-
-// const anotherPerson: Person={
-//         name: '',
-//         age: 0
-// };
-
-//<---------------- “keyof” Index Type Queries
-
-// const person={
-//     name: 'Joaquin',
-//     age: 24
-// };
-
-// type Person = typeof person; 
-
-// type PersonKeys = keyof Person; // name | age
-
-// type PersonTyoes = Person[PersonKeys];
-
-// const anotherPerson: Person={
-//         name: '',
-//         age: 0
-// };
-
-
-//<----------------  “keyof”, Generics and Lookup Types
-
-
-const person={
+const person: Person = {
     name: 'Joaquin',
     age: 24
 };
 
-type Person = typeof person; 
 
-type PersonKeys = keyof Person; // name | age
-
-type PersonTyoes = Person[PersonKeys];
-
-function getProperty<T, K extends keyof T>(obj: T, key: K){
-        return obj[key];
+type MyReadOnly<T>={
+    readonly[P in keyof T]: T[P];
 }
 
-const personName = getProperty(person, 'name'); 
+function freeze<T>(obj: T): MyReadOnly<T>{
+return Object.freeze(obj);
+}
 
-console.log(personName);
-
-const anotherPerson: Person={
-        name: '',
-        age: 0
-};
+const newPerson = freeze(person);
