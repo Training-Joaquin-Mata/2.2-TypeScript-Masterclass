dcff72973e1ea2ce181b3d321b531e446cc3aa15
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-29 14:15:31 -0500

    Partial Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,49 @@
-//<---------------- ReadOnly Mapped Type
+// //<---------------- ReadOnly Mapped Type
+// interface Person{
+//     name: string;
+//     age: number;
+// };
+
+// const person: Person = {
+//     name: 'Joaquin',
+//     age: 24
+// };
+
+
+// type MyReadOnly<T>={
+//     readonly[P in keyof T]: T[P];
+// }
+
+// function freeze<T>(obj: T): MyReadOnly<T>{
+// return Object.freeze(obj);
+// }
+
+// const newPerson = freeze(person);
+
+//<---------------- Partial Mapped Type
 interface Person{
     name: string;
     age: number;
 };
 
+type MyPartial<T> ={
+    [P in keyof T]?: T[P];
+}
+
+function updatePerson(person: Person, prop: MyPartial<Person>){
+    return {...person, ...prop}
+}
+
 const person: Person = {
     name: 'Joaquin',
     age: 24
 };
+console.log(person)
+const upP=updatePerson(person, {name: 'name', age: 23} );
+updatePerson(person, {name: 'name', age: 23} );
+
+console.log(person)
+console.log(upP);
 
 
-type MyReadOnly<T>={
-    readonly[P in keyof T]: T[P];
-}
 
-function freeze<T>(obj: T): MyReadOnly<T>{
-return Object.freeze(obj);
-}
-
-const newPerson = freeze(person);
