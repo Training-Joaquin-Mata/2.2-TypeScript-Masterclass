58968625d8d63063f3a3db7a70053dfc29a6a43c
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-29 13:42:21 -0500

    “keyof”, Generics and Lookup Types

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,26 @@
 
 //<---------------- “keyof” Index Type Queries
 
+// const person={
+//     name: 'Joaquin',
+//     age: 24
+// };
+
+// type Person = typeof person; 
+
+// type PersonKeys = keyof Person; // name | age
+
+// type PersonTyoes = Person[PersonKeys];
+
+// const anotherPerson: Person={
+//         name: '',
+//         age: 0
+// };
+
+
+//<----------------  “keyof”, Generics and Lookup Types
+
+
 const person={
     name: 'Joaquin',
     age: 24
@@ -25,8 +45,15 @@ type PersonKeys = keyof Person; // name | age
 
 type PersonTyoes = Person[PersonKeys];
 
+function getProperty<T, K extends keyof T>(obj: T, key: K){
+        return obj[key];
+}
+
+const personName = getProperty(person, 'name'); 
+
+console.log(personName);
+
 const anotherPerson: Person={
         name: '',
         age: 0
 };
-
