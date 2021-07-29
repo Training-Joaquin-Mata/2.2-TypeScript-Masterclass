45b7abd7d1da44c3fa72965c41ee6e565f776085
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-29 13:29:42 -0500

    “typeof” Type Queries

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,11 @@
-// class MyClass{
+const person={
+    name: 'Joaquin',
+    age: 24
+};
 
-//     myMethod(){
-//         console.log('Class:::', this);
-//     }
-// }
+type Person = typeof person; 
 
-// const myInstance = new MyClass();
-
-// myInstance.myMethod();
-
-
-// //Object Literal
-
-// const myObj={
-//     myMethod(){
-//         console.log('Object:::', this);
-//     },
-// }
-
-// myObj.myMethod();
-
-// // Function
-
-// function myFunction(...text: string[]): any{
-//     console.log('Function:::', this, text);
-// }
-
-// const bindFunction = myFunction.bind(myObj, 'ABC', 'DEF');
-// bindFunction();
-// myFunction().call(myObj,'ABC', 'DEF' );
-// myFunction().apply(myObj,['ABC', 'DEF']);
-
-
-// class MyClass{
-//     myMethod(){
-//         const foo =123;
-
-//         console.log('1', this);
-//         setTimeout(()=>{
-//             console.log(foo);
-//             console.log(this);
-//             //lexical scope
-//         }, 0)
-//     }
-// }
-
-
-// const myInstance = new MyClass();
-
-// myInstance.myMethod();
-
-
-const elem = document.querySelector('.click');
-
-function handleClick(this: HTMLAnchorElement, event: Event){
-        event.preventDefault();
-        console.log(this.addEventListener);
-}
-
-elem.addEventListener('click', handleClick, false);
-
-
-
+const anotherPerson: Person={
+        name: '',
+        age: 0
+};
