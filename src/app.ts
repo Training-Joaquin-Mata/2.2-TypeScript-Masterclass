bb60eea108c861a2feff73d5c3476def8605c6b5
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-30 10:45:43 -0500

    “Required” Mapped Type, +/- Modifiers

--- src/app.ts
+++ src/app.ts
@@ -21,29 +21,55 @@
 // const newPerson = freeze(person);
 
 //<---------------- Partial Mapped Type
+
+// // el partial hace que los campos dentro de la interfaz sean opcionales, les agrega el ? 
+// interface Person{
+//     name: string;
+//     age: number;
+// };
+
+// type MyPartial<T> ={
+//     [P in keyof T]?: T[P];
+// }
+
+// function updatePerson(person: Person, prop: MyPartial<Person>){
+//     return {...person, ...prop}
+// }
+
+// const person: Person = {
+//     name: 'Joaquin',
+//     age: 24
+// };
+// console.log(person)
+// const upP=updatePerson(person, {name: 'name', age: 23} );
+// updatePerson(person, {name: 'name', age: 23} );
+
+// console.log(person)
+// console.log(upP);
+
+
+//<---------------- “Required” Mapped Type, +/- Modifiers
+
+
+// El required hace que los campós que esten dentro de la interfaz sean obligarorios, le quita el ? que los hace opcionales
+
 interface Person{
     name: string;
-    age: number;
+    age?: number;
 };
 
-type MyPartial<T> ={
-    [P in keyof T]?: T[P];
+
+
+function printAge(person: Required<Person>){
+        return `${person.name} is ${person.age}`
 }
 
-function updatePerson(person: Person, prop: MyPartial<Person>){
-    return {...person, ...prop}
-}
-
-const person: Person = {
+const person: Required<Person> = {
     name: 'Joaquin',
     age: 24
 };
-console.log(person)
-const upP=updatePerson(person, {name: 'name', age: 23} );
-updatePerson(person, {name: 'name', age: 23} );
 
-console.log(person)
-console.log(upP);
+const age = printAge(person);
 
-
+console.log(age);
 
