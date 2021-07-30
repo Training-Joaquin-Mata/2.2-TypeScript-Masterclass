277ca33e8b4393415510e5fa93abb716a9f7db3f
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-30 15:30:59 -0500

    Literal TypeGuards and "in" operator

--- src/app.ts
+++ src/app.ts
@@ -77,25 +77,64 @@
 //                 ${playListName}                
 // `);
 
-// <----------------------- User Defined Type Guards
+// // <----------------------- User Defined Type Guards
 
 
+// class Song {
+//     constructor(public title: string, public duration: number){}
+// }
+
+// class PlayList {
+//     constructor(public name: string, public songs: Song[]){}
+// }
+
+
+// //esta nueva funcion solo puede regresar un boolean, esta funcion se usa para comprobar si el tem es de cirtto tipo, y si lo es manda un true
+// function isSong(item: any): item is Song{
+//     return item instanceof Song;
+// }
+
+// function getItemName(item: Song | PlayList){
+//     if(isSong(item)){
+//         return item.title;
+//     }
+
+//     return item.name;
+// }
+
+// const songName = getItemName(new Song('Wonderful Wonderful', 300000))
+
+// console.log(`Name of the song: 
+//                 ${songName}                
+// `);
+
+// const playListName = getItemName(
+//     new PlayList('The Best Songs', [new Song('The man', 3000000)])
+// ); 
+
+// console.log(`PlayList name:  
+//                 ${playListName}                
+// `);
+
+
+// <----------------------- Literal TypeGuards and "in" operator
 class Song {
-    constructor(public title: string, public duration: number){}
+    kind:'song',
+    constructor(public title: string, public duration: number, song){}
 }
 
 class PlayList {
     constructor(public name: string, public songs: Song[]){}
 }
 
-
-//esta nueva funcion solo puede regresar un boolean, esta funcion se usa para comprobar si el tem es de cirtto tipo, y si lo es manda un true
 function isSong(item: any): item is Song{
-    return item instanceof Song;
+    // "in" checa si la propiedad se encuentra en el objeto, regresa true si lo esta y false si no
+    return 'title' in item ;
 }
 
 function getItemName(item: Song | PlayList){
-    if(isSong(item)){
+    //if(isSong(item)){
+      if(item.kind === 'song'){
         return item.title;
     }
 
