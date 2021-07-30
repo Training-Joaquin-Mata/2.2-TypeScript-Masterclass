56e9da983d820b8225a0cf31253224d55f22d973
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-30 16:12:45 -0500

    Discriminated (Tagged) Unions

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,41 @@
+// // <-----------------  Intersection Types
+// interface Order{
+//     id: string;
+//     amount: number;
+//     currency: string;
+// }
 
-// <-----------------  Intersection Types
+// interface Stripe{
+//     card: string;
+//     cvc: string;
+// }
+
+// interface PayPal{
+//     email: string;
+// }
+
+// type CheckoutCard = Order  & Stripe;
+// type CheckoutPayPal= Order & PayPal;
+// // type CheckoutABC= Order & {name: string .....};
+
+// const order: Order = {
+//     id: 'afcadf',
+//     amount: 100,
+//     currency: 'USD'
+// }
+
+// const orderCard: CheckoutCard= { 
+//    ...order,
+//     card:'1111 2222 3333 4444', 
+//     cvc: '123'
+// }
+
+// const orderPayPal: CheckoutPayPal={
+//     ...order,
+//     email:'something@something.com'
+// }
+
+// <-----------------  Discriminated (Tagged) Unions
 interface Order{
     id: string;
     amount: number;
@@ -7,17 +43,18 @@ interface Order{
 }
 
 interface Stripe{
+    type: 'stripe';
     card: string;
     cvc: string;
 }
 
 interface PayPal{
+    type: 'paypal';
     email: string;
 }
 
 type CheckoutCard = Order  & Stripe;
 type CheckoutPayPal= Order & PayPal;
-// type CheckoutABC= Order & {name: string .....};
 
 const order: Order = {
     id: 'afcadf',
@@ -27,11 +64,28 @@ const order: Order = {
 
 const orderCard: CheckoutCard= { 
    ...order,
+   type: 'stripe',
     card:'1111 2222 3333 4444', 
     cvc: '123'
 }
 
 const orderPayPal: CheckoutPayPal={
     ...order,
+    type: 'paypal',
     email:'something@something.com'
+}
+
+// creamops un union type, y despues podemos discriminar cual queremos usar
+
+type Payload = CheckoutCard | CheckoutPayPal;
+
+function checkout(payload: Payload){
+
+    if(payload.type=== 'stripe' ){
+        console.log(`${payload.card}, ${payload.cvc}`)
+    }
+    if(payload.type=== 'paypal' ){
+        console.log(`${payload.email}}`)
+    }
+        
 }
