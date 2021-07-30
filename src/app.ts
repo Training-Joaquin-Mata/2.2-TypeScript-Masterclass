dbb56c79a9a126f88006a8f29ec0bddb3b6a6cc8
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-07-30 11:42:38 -0500

    “Pick” Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -51,25 +51,49 @@
 //<---------------- “Required” Mapped Type, +/- Modifiers
 
 
-// El required hace que los campós que esten dentro de la interfaz sean obligarorios, le quita el ? que los hace opcionales
+// // El required hace que los campós que esten dentro de la interfaz sean obligarorios, le quita el ? que los hace opcionales
+
+// interface Person{
+//     name: string;
+//     age?: number;
+// };
+
+
+
+// function printAge(person: Required<Person>){
+//         return `${person.name} is ${person.age}`
+// }
+
+// const person: Required<Person> = {
+//     name: 'Joaquin',
+//     age: 24
+// };
+
+// const age = printAge(person);
+
+// console.log(age);
+
+
+//<---------------- “Pick” Mapped Type
+
+// Es como decirle al compilador que solo estas usando un set especifico de keys
+// Lo que hace pick es regresarnos una seleccion de las propiedades del objeto
 
 interface Person{
     name: string;
-    age?: number;
+    age: number;
+    address: {}
 };
 
-
-
-function printAge(person: Required<Person>){
-        return `${person.name} is ${person.age}`
+type MyPick<T, K extends keyof T> = {
+    [P in  K]: T[P]; 
 }
 
-const person: Required<Person> = {
+const person: MyPick<Person, 'name'| 'age' > = {
     name: 'Joaquin',
     age: 24
 };
 
-const age = printAge(person);
 
-console.log(age);
+
 
