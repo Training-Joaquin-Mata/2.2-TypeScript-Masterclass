e38d9a9bb83d5316eac1521bf835de6b0cef60d5
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-03 16:55:36 -0500

    Emitting Declaration Files

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
-import * as _ from 'lodash'
-//<-------------------- Writing Declaration Files
+// import * as _ from 'lodash'
+// //<-------------------- Writing Declaration Files
 
-_.chunk([1,2,3,4], 2); //[[1,2].[3,4]]
+// _.chunk([1,2,3,4], 2); //[[1,2].[3,4]]
 
-_.mixin({
-    log(item: string){
-        console.log(':::', item);
-    }
-})
+// _.mixin({
+//     log(item: string){
+//         console.log(':::', item);
+//     }
+// })
 
-_.log('Hello');
+// _.log('Hello');
+
+//<-------------------- Emitting Declaration Files
+
+export class Foo{
+
+    constructor(public name: string){}
+    bar (age: number){}
+}
+
