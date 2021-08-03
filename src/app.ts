7a7dc03362b8f739dc0dc94c7baecc978da84633
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-03 10:37:06 -0500

    Interfaces vs Classes

--- src/app.ts
+++ src/app.ts
@@ -90,28 +90,46 @@
         
 // }
 
-// <-----------------  Interfaces vs Type Aliases
+// // <-----------------  Interfaces vs Type Aliases
 
-interface Item{
-    name: string;
+// interface Item{
+//     name: string;
+// }
+
+// interface Artist extends Item{
+//     songs: number,
+// }
+
+// interface Artist{
+//     getSongs(): number;
+// }
+
+// type Artist2={name: string} & Item;
+
+
+
+// const newArtist: Artist={
+//     name:'abc',
+//     songs:5,
+//     getSongs(){
+//         return this.songs;
+//     }
+// }
+
+// <-----------------  Interfaces vs lasses
+
+// interface Artist{
+//     name: string
+// }
+
+class ArtistCreator /*implements Artist*/{
+    constructor(public name: string){}
 }
 
-interface Artist extends Item{
-    songs: number,
+function artistFactory({ name }: ArtistCreator){
+
+    return new ArtistCreator(name);
 }
 
-interface Artist{
-    getSongs(): number;
-}
 
-type Artist2={name: string} & Item;
-
-
-
-const newArtist: Artist={
-    name:'abc',
-    songs:5,
-    getSongs(){
-        return this.songs;
-    }
-}
+artistFactory ({name: 'Joaquin'})
