3e3c28142e7120171e27ebac052e7d2e4eaec8e1
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-03 13:45:12 -0500

    String Enums and Inlining Members

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,44 @@
-//<-------------------- Numeric Enums and reverse mappings
+// //<-------------------- Numeric Enums and reverse mappings
 
-enum Sizes{
-    Small,
-    Medium, 
-    Large,
+// enum Sizes{
+//     Small,
+//     Medium, 
+//     Large,
+// }
+
+// enum Sizes{
+//     ExtraLarge = 3
+// }
+
+// const selectedSize = 2;
+
+// console.log(Sizes);
+// // String Value
+// console.log(Sizes[selectedSize]);
+// // Numeric Value
+// console.log(Sizes.Medium);
+// // String Value
+// console.log(Sizes[Sizes.Large]);
+// // Numeric Value
+// console.log(Sizes['Small']);
+
+
+//<-------------------- String Enums and Inlining Members
+
+const enum Sizes{
+    Small = 'small', 
+    Medium = 'medium',
+    Large = 'large',
+
+}
+let selected: Sizes = Sizes.Small;
+
+function updateSize(size: Sizes): void{
+    selected= size;
 }
 
-enum Sizes{
-    ExtraLarge = 3
-}
+updateSize(Sizes.Large);
 
-const selectedSize = 2;
+console.log(selected);
 
-console.log(Sizes);
-// String Value
-console.log(Sizes[selectedSize]);
-// Numeric Value
-console.log(Sizes.Medium);
-// String Value
-console.log(Sizes[Sizes.Large]);
-// Numeric Value
-console.log(Sizes['Small']);
+
