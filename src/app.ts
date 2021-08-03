cb33f55e7f2437342321d7252e321d0dbcb754ed
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-03 09:56:50 -0500

    Interfaces vs Type Aliases

--- src/app.ts
+++ src/app.ts
@@ -35,57 +35,83 @@
 //     email:'something@something.com'
 // }
 
-// <-----------------  Discriminated (Tagged) Unions
-interface Order{
-    id: string;
-    amount: number;
-    currency: string;
-}
+// // <-----------------  Discriminated (Tagged) Unions
+// interface Order{
+//     id: string;
+//     amount: number;
+//     currency: string;
+// }
 
-interface Stripe{
-    type: 'stripe';
-    card: string;
-    cvc: string;
-}
+// interface Stripe{
+//     type: 'stripe';
+//     card: string;
+//     cvc: string;
+// }
 
-interface PayPal{
-    type: 'paypal';
-    email: string;
-}
+// interface PayPal{
+//     type: 'paypal';
+//     email: string;
+// }
 
-type CheckoutCard = Order  & Stripe;
-type CheckoutPayPal= Order & PayPal;
+// type CheckoutCard = Order  & Stripe;
+// type CheckoutPayPal= Order & PayPal;
 
-const order: Order = {
-    id: 'afcadf',
-    amount: 100,
-    currency: 'USD'
-}
+// const order: Order = {
+//     id: 'afcadf',
+//     amount: 100,
+//     currency: 'USD'
+// }
 
-const orderCard: CheckoutCard= { 
-   ...order,
-   type: 'stripe',
-    card:'1111 2222 3333 4444', 
-    cvc: '123'
-}
+// const orderCard: CheckoutCard= { 
+//    ...order,
+//    type: 'stripe',
+//     card:'1111 2222 3333 4444', 
+//     cvc: '123'
+// }
 
-const orderPayPal: CheckoutPayPal={
-    ...order,
-    type: 'paypal',
-    email:'something@something.com'
-}
+// const orderPayPal: CheckoutPayPal={
+//     ...order,
+//     type: 'paypal',
+//     email:'something@something.com'
+// }
 
-// creamops un union type, y despues podemos discriminar cual queremos usar
+// // creamops un union type, y despues podemos discriminar cual queremos usar
 
-type Payload = CheckoutCard | CheckoutPayPal;
+// type Payload = CheckoutCard | CheckoutPayPal;
 
-function checkout(payload: Payload){
+// function checkout(payload: Payload){
 
-    if(payload.type=== 'stripe' ){
-        console.log(`${payload.card}, ${payload.cvc}`)
-    }
-    if(payload.type=== 'paypal' ){
-        console.log(`${payload.email}}`)
-    }
+//     if(payload.type=== 'stripe' ){
+//         console.log(`${payload.card}, ${payload.cvc}`)
+//     }
+//     if(payload.type=== 'paypal' ){
+//         console.log(`${payload.email}}`)
+//     }
         
+// }
+
+// <-----------------  Interfaces vs Type Aliases
+
+interface Item{
+    name: string;
+}
+
+interface Artist extends Item{
+    songs: number,
+}
+
+interface Artist{
+    getSongs(): number;
+}
+
+type Artist2={name: string} & Item;
+
+
+
+const newArtist: Artist={
+    name:'abc',
+    songs:5,
+    getSongs(){
+        return this.songs;
+    }
 }
